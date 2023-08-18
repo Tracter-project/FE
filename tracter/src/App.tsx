c91aab3ba94859d9b230b7261053c3f843f2a225
zero86 <katanazero86@gmail.com>
2023-08-18 16:38:20 +0900

    fix/setting - web fonts 적용 및 index.css 삭제

--- tracter/src/App.tsx
+++ tracter/src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
     <Container>
       <Header /> 
       <Outlet />
-      <Footer /> 
+      <Footer />
     </Container>
   );
 }
--- tracter/src/main.tsx
+++ tracter/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from './App.tsx'
+import '../src/assets/styles/global.scss';
 import NotFound from './pages/test/NotFound.tsx';
 import Home from './pages/test/Home/Home.tsx';
 import Login from './pages/test/Login/Login.tsx';
