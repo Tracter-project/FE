e89150e7c4f0fb58af62c36e735d0df91c10fc4d
[SW5]이해솔 <hsler404@gmail.com>
2023-08-19 18:31:03 +0900

    Feat: CommentView 컴포넌트

--- src/pages/sol.tsx
+++ src/pages/sol.tsx
@@ -1,11 +1,13 @@
 import Comment from "../components/Comment/Comment";
-// import CommentView from "../components/Comment/Comment";
+import CommentView from "../components/CommentView/CommentView";
 
 export default function Sol() {
   return (
     <>
-      <Comment>댓글 작성</Comment>
-      {/* <CommentView></CommentView> */}
+      {/* <Comment>댓글 작성</Comment> */}
+      <CommentView></CommentView>
+      <CommentView></CommentView>
+      <CommentView></CommentView>
     </>
   );
 }
