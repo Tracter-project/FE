b04eef7c770e1c99e81ffc01f5d56c6b1f34553c
[SW5]이해솔 <hsler404@gmail.com>
2023-08-20 00:42:29 +0900

    Feat: PostTitle,Content Component

---
+++ src/components/PostTitleInput/PostTitleInput.tsx
@@ -0,0 +1,11 @@
+// import React from "react";
+import styles from "./PostTitleInput.module.scss";
+
+// onClick={}
+export default function PostTitleInput() {
+  return (
+    <>
+      <input type="text" className={styles.postTitleInput} />
+    </>
+  );
+}
