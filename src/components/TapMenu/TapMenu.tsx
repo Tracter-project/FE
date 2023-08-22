dc0017e3188c9147d6b19b206157d6bcbbcb525b
[SW5] 조민호 <miinnnhho>
2023-08-23 01:08:06 +0900

    docs: Tapmenu 수정

--- src/components/TapMenu/TapMenu.tsx
+++ src/components/TapMenu/TapMenu.tsx
@@ -1,34 +1,37 @@
-// TapMenu.tsx
-
 import React from 'react';
 import SimpleTap, { Tab, TabProps } from './SimpleTap';
 import './TapMenu.module.scss';
 
 const tabs: TabProps[] = [
-  {
-    aKey: '숙소',
-    title: '숙소 칸',
-    children: '숙소 내용',
-  },
-  {
-    aKey: '카테고리',
-    title: '카테고리 칸',
-    children: '카테고리 내용',
-  },
+    {
+        aKey: '숙소',
+        title: '숙소 칸',
+        children: (
+            <div>
+                <h3>숙소 내용에 대한 값</h3>
+                <p>추가 설명 뭐시기뭐시기</p>
+            </div>
+        ),
+    },
+    {
+        aKey: '카테고리',
+        title: '카테고리 칸',
+        children: '카테고리 내용',
+    },
 ];
 
-const TapMenu: React.FC = () => {
-  return (
-    <div>
-      <SimpleTap>
-        {tabs.map((item) => (
-          <Tab key={item.aKey} aKey={item.aKey} title={item.title}>
-            {item.children}
-          </Tab>
-        ))}
-      </SimpleTap>
-    </div>
-  );
+const TapMenu = (): JSX.Element | null => {
+    return (
+        <div>
+            <SimpleTap>
+                {tabs.map((item) => (
+                    <Tab key={item.aKey} aKey={item.aKey} title={item.title}>
+                        {item.children}
+                    </Tab>
+                ))}
+            </SimpleTap>
+        </div>
+    );
 };
 
 export default TapMenu;
