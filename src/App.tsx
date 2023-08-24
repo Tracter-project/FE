000fd0dad3baf179c66fb6ac34dec39147c03860
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2023-08-24 18:49:29 +0900

    feat: recoil setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Container from "./components/Container/Container.tsx";
+import { RecoilRoot } from "recoil";
 
 export default function App() {
     return (
-        <>
+        <RecoilRoot>
             <Header />
             <Container>
                 <Outlet />
             </Container>
             <Footer />
-        </>
+        </RecoilRoot>
     );
 }
