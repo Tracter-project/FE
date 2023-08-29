2b40b7549fda7bcad7f9543a6564d267470d5537
[SW5] 조민호 <miinnnhho>
2023-08-29 14:04:04 +0900

    추가작업

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,21 @@
-import React from "react";
-import styles from "./Button.module.scss";
-import Title from "../Title/Title";
+import React from 'react';
+import styles from './Button.module.scss';
+import Title from '../Title/Title';
 
 interface ButtonProps {
-  children: React.ReactNode;
-  onClick: () => void;
+    children: React.ReactNode;
+    onClick: () => void;
 }
 
+const testResult = () => {
+  
+};
 export default function Button({ children, onClick }: ButtonProps) {
-  return (
-    <>
-      <button className={styles.button} onClick={onClick}>
-        <Title size="p">{children}</Title>
-      </button>
-    </>
-  );
+    return (
+        <>
+            <button className={styles.button} onClick={onClick}>
+                <Title size="p">{children}</Title>
+            </button>
+        </>
+    );
 }
