b98dad912d81d7772ec66fee91549f5412df9001
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2023-08-30 13:59:19 +0900

    fix: api테스트

--- src/components/AdminTable/AdminTable.tsx
+++ src/components/AdminTable/AdminTable.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axiosRequest from "../../api";
 import Title from "../Title/Title";
 import AddButton from "../AddButton/AddButton";
 import ModifyButton from "../ModifyButton/ModifyButton";
@@ -59,17 +60,63 @@ export default function AdminTable({ data, setData }: AdminTableProps) {
     const handleSelect = (region: string, category: string) => {
         setSelectedRegion(region);
         setSelectedCategory(category);
+        console.log(region, category);
+    };
+    const [selectedIds, setSelectedIds] = useState<number[]>([]);
+    // 선택한 아이디 업데이트
+    useEffect(() => {
+        const selected = data
+            .filter((item) => item.selected)
+            .map((item) => item.id);
+        setSelectedIds(selected);
+    }, [data]);
+    // 숙소 수정 요청
+    const handleEdit = async () => {
+        // 선택한 숙소들의 아이디 리스트(selectedIds)를 활용하여 API 호출
+        try {
+            const response = await axiosRequest.requestAxios(
+                "patch",
+                "/admin/places",
+                {
+                    placeIds: selectedIds,
+                    // 추가적인 수정 데이터들을 보내주세요
+                }
+            );
+            console.log("숙소 수정 성공:", response);
+            // 수정 성공 시 처리
+        } catch (error) {
+            console.error("숙소 수정 실패:", error);
+            // 에러 처리
+        }
     };
 
+    // 숙소 삭제 요청
+    const handleDelete = async () => {
+        // 선택한 숙소들의 아이디 리스트(selectedIds)를 활용하여 API 호출
+        try {
+            const response = await axiosRequest.requestAxios(
+                "delete",
+                "/admin/places",
+                {
+                    placeIds: selectedIds,
+                }
+            );
+            console.log("숙소 삭제 성공:", response);
+            // 삭제 성공 시 처리
+        } catch (error) {
+            console.error("숙소 삭제 실패:", error);
+            // 에러 처리
+        }
+    };
     return (
         <>
             <div className={styles.tableWrap}>
                 <div className={styles.buttonIcon}>
                     <AddButton onClick={handleOpenModal}></AddButton>
 
-                    <ModifyButton onClick={() => {}}></ModifyButton>
+                    <ModifyButton onClick={handleEdit}></ModifyButton>
 
-                    <DeleteButton onClick={() => {}}></DeleteButton>
+                    <DeleteButton onClick={handleDelete}></DeleteButton>
                 </div>
                 <table className={styles.table}>
                     <thead>
--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axiosRequest from "../../api";
 import styles from "./Admin.module.scss";
 import Title from "../../components/Title/Title";
 import AdminTable from "../../components/AdminTable/AdminTable";
@@ -7,29 +8,61 @@ import AdminTable from "../../components/AdminTable/AdminTable";
 import TabButton from "../../components/TabButton/TabButton";
 import EditModal from "../../components/EditModal/EditModal";
 
+interface Place {
+    id: number;
+    selected: boolean;
+    imageUrl: string;
+    area: string;
+    category: string;
+    name: string;
+    description: string;
+    price: number;
+}
+interface PlaceResponse {
+    status: number;
+    message: string;
+    totalPlace: Place[]; // Place 타입의 배열을 예상
+    // 다른 필드들도 정의해야 함
+}
 export default function Admin() {
-    const [data, setData] = useState([
-        {
-            id: 1,
-            selected: false,
-            imageUrl: "URL_1",
-            area: "서울",
-            category: "호텔",
-            name: "숙소 1",
-            description: "숙소 설명 1",
-            price: 100,
-        },
-        {
-            id: 2,
-            selected: false,
-            imageUrl: "URL_2",
-            area: "제주",
-            category: "펜션",
-            name: "숙소 2",
-            description: "숙소 설명 2",
-            price: 150,
-        },
-    ]);
+    // const [data, setData] = useState([
+    //     {
+    //         id: 1,
+    //         selected: false,
+    //         imageUrl: "URL_1",
+    //         area: "서울",
+    //         category: "호텔",
+    //         name: "숙소 1",
+    //         description: "숙소 설명 1",
+    //         price: 100,
+    //     },
+    //     {
+    //         id: 2,
+    //         selected: false,
+    //         imageUrl: "URL_2",
+    //         area: "제주",
+    //         category: "펜션",
+    //         name: "숙소 2",
+    //         description: "숙소 설명 2",
+    //         price: 150,
+    //     },
+    // ]);
+    const [data, setData] = useState<Place[]>([]);
+
+    useEffect(() => {
+        // 전체 숙소 데이터 조회하는 API 호출
+        async function fetchPlaces() {
+            try {
+                const placesResponse: PlaceResponse =
+                    await axiosRequest.requestAxios("get", "/places/all");
+                setData(placesResponse.totalPlace); // 서버 응답에서 실제 숙소 데이터를 추출하여 설정
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchPlaces();
+    }, []);
 
     return (
         <>
@@ -37,13 +70,7 @@ export default function Admin() {
                 관리자 페이지(숙소리스트)
             </Title>
             <TabButton></TabButton>
-            <AdminTable
-                data={data}
-                // onEdit={handleEdit}
-                // onDelete={handleDelete}
-                // onAdd={handleAdd}
-                setData={setData}
-            ></AdminTable>
+            <AdminTable data={data} setData={setData}></AdminTable>
             {/* <DeleteModal className={styles.content}>
                 <Title size="h2">숙소리스트 삭제</Title>
                 <Title size="b">숙소리스트를 삭제하시겠습니까?</Title>
