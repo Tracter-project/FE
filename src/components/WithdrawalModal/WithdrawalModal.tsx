55b65b998faa32fdee838716c61cd4d573f71e23
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2023-09-01 18:52:55 +0900

    fix/page

--- src/components/WithdrawalModal/WithdrawalModal.tsx
+++ src/components/WithdrawalModal/WithdrawalModal.tsx
@@ -4,19 +4,19 @@ import Title from "../Title/Title";
 import Button from "../Button/Button";
 
 interface ModalProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 export default function WithdrawalModal({ children }: ModalProps) {
-  return (
-    <>
-      <div className={styles.modalBox}>
-        <Title size="h2">{children}</Title>
-        <div className={styles.modalButton}>
-          <Button>확인</Button>
-          <Button>취소</Button>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={styles.modalBox}>
+                <Title size="h2">{children}</Title>
+                <div className={styles.modalButton}>
+                    {/* <Button>확인</Button>
+          <Button>취소</Button> */}
+                </div>
+            </div>
+        </>
+    );
 }
