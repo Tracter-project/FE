481b33a3e28ee983feac4dc47d6d732734c7b36d
zero86 <katanazero86@gmail.com>
2023-09-01 22:54:35 +0900

    fix 선택 하면서, 초기화

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -1,11 +1,11 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import styles from "./Category.module.scss";
 import Title from "../../components/Title/Title";
 import MainImage from "../../components/MainImage/MainImage";
 import DropdownOption from "../../components/DropdownOption/DropdownOption";
-import { Link, useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
-import axiosRequest from "../../api"; // axios 라이브러리를 import
 import { AxiosResponse } from "axios";
+import axiosRequest from "../../api"; // axios 라이브러리를 import
 
 interface IResponse {
   data: MainImageItem[];
@@ -27,7 +27,6 @@ interface Category {
 export default function Category() {
   const params = useParams();
   const categoryId = Number(params.categoryId);
-  console.log(categoryId, typeof categoryId);
   const [imageList, setImageList] = useState<MainImageItem[]>([]);
   const [categories, setCategories] = useState<Category[]>([]);
   const [selectedCategoryName, setSelectedCategoryName] = useState<
@@ -35,16 +34,14 @@ export default function Category() {
   >(null);
 
   useEffect(() => {
-    console.log("전체조회다", categories);
     const fetchCategories = async () => {
       try {
         const categoryResponse: AxiosResponse = await axiosRequest.requestAxios(
           "get",
           "/categories"
         );
-
+        console.log(categoryResponse.data);
         setCategories(categoryResponse.data);
-        console.log("카테고리", categoryResponse.data);
       } catch (error) {
         console.error("카테고리 조회 실패:", error);
       }
@@ -80,6 +77,7 @@ export default function Category() {
     };
 
     if (selectedCategoryName !== null) {
+      setImageList([]);
       fetchCategoryImages();
     }
   }, [selectedCategoryName]);
